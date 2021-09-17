00615f19e815c40f7ad58e59d741ed597813ca61
miro5460 <robin411989@gmail.com>
2021-09-17 08:01:29 -0400

    updated project name in pom, midway through chapter 6

---
+++ jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,71 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equaulity() {
+        Expr expr = comparision();
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr; 
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(ToeknType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous;
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current = 1);
+    }
+}
